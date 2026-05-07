537d86e9b784a8c6eb24e2623512b12eef90bbea
Kaushik <algorithmunloack@gmail.com>
2026-05-07 11:58:44 +0530

    Refactor linear layer example to use 2 input features and update comments for clarity

--- Core/linear.py
+++ Core/linear.py
@@ -5,15 +5,24 @@
  # nn.linear (4,2) this take 4 number in ,spit 2 number out 
  # It has two main parameters : in_features and out_features . In features is the number of input features and out features is the number of output features . So if you have a layer with in features 10 and out features 5 , it means that this layer will take in a vector of 10 numbers and output a vector of 5 numbers . The weights and bias are learnable parameters that the model adjusts during training to minimize the loss function .
 
+# Here we took a vector of 10 numbers and squashed them down into a single value using some learned weigth and a bias .
+# Linear layers  expectes floats(decimal numbers) as input and output . So if you give it integers it will automatically convert them to floats before processing . 
 import torch 
 import time 
 import torch.nn as nn 
-layers = nn.Linear(in_features=10,out_features=1)
+layers = nn.Linear(in_features=2,out_features=1)
+# When we defined the layers nn.Linear then it automatically created the weights and the bias for us .
 print(layers)
 print("\n")
-actual_input = torch.tensor([[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]])
+actual_input = torch.tensor([[1.0,2.0]])
 # Passing it thorugh the linear layer
-output = layers(actual_input)
+output = layers(actual_input)  # When we ran this line then performed a dot product plus an addition .
 print("Input :", actual_input)
 print ("\n")
 print("Output :", output)
+print("\n")
+print("The weights :", layers.weight)
+print("\n")
+print("The bias :", layers.bias)
+
+# During training: We keep everything in RAM because it's thousands of times faster than a DB.After training: We store the Weights in a file.After making a prediction: We store the Final Result in a DB for your app to use.
