4ea353bcaa22cf7e0a425a3fde4c89cde9a7340e
Kaushik <algorithmunloack@gmail.com>
2026-05-09 22:07:38 +0530

    The code is uploaded here

---
+++ saving.py
@@ -0,0 +1,9 @@
+import torch
+
+# Load full checkpoint
+checkpoint = torch.load("10M/checkpoint_epoch3.pt", map_location="cpu")
+
+# Save only model weights
+torch.save(checkpoint['model_state_dict'], "10M/model_weights.pt")
+
+print("Saved stripped model: model_weights.pt")
